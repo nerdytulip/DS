55a740b0a8d333a771fbe973c7de74bd9893e90d
parulraich <parul.raich@gmail.com>
2019-11-21 13:49:50 +0530

    hackerearth question NCR-correct solution updated

--- src/com/company/dp.java
+++ src/com/company/dp.java
@@ -223,30 +223,50 @@ public class dp {
         return map;
     }
 
-    /*Longest increasing subset
+    /*Longest increasing subsequence
     * i for traversing the array elements and j for traversing till i
     * if(arr[i]>arr[j]{T[i]=max(T[i],T[j]+1)}
     * O(n^2)
     * */
     static int LIS(int arr[],int n){
-        int lis[] = new int[n];
-        for(int i =0;i<n;i++){
-            lis[i]=1;
+        int T[] = new int[arr.length];
+        int actualSolution[] = new int[arr.length];
+        for(int i=0; i < arr.length; i++){
+            T[i] = 1;
+            actualSolution[i] = i;
         }
-        for(int i=1;i<n;i++){
-            for(int j =0;j<i;i++){
-                if(arr[i]>arr[j] && lis[i]<lis[j]+1){
-                    lis[i]=lis[j]+1;
+
+        for(int i=1; i < arr.length; i++){
+            for(int j=0; j < i; j++){
+                if(arr[i] > arr[j]){
+                    if(T[j] + 1 > T[i]){
+                        T[i] = T[j] + 1;
+                        //set the actualSolution to point to guy before me
+                        actualSolution[i] = j;
+                    }
                 }
             }
         }
-        int max =0;
-        for(int i=0;i<n;i++){
-            if(max<lis[i])
-                max=lis[i];
+
+        //find the index of max number in T
+        int maxIndex = 0;
+        for(int i=0; i < T.length; i++){
+            if(T[i] > T[maxIndex]){
+                maxIndex = i;
+            }
         }
 
-     return max;
+        //lets print the actual solution
+        int t = maxIndex;
+        int newT = maxIndex;
+        do{
+            t = newT;
+            System.out.print(arr[t] + " ");
+            newT = actualSolution[t];
+        }while(t != newT);
+        System.out.println();
+
+        return T[maxIndex];
     }
 
     //TC-O(n)
@@ -420,7 +440,7 @@ public class dp {
         int n = Y.length();
         System.out.println(LCS(X, Y, m, n));*/
 
-        int arr1[]={2,3,7,8,10};
+      //  int arr1[]={2,3,7,8,10};
        // System.out.println(SubsetSum(arr1,2));
        /* boolean c[][];
         c=SubsetSum(arr1,11);
@@ -448,6 +468,8 @@ public class dp {
        String str2="abcdef";
        int result=dynamicEditDistance(str1.toCharArray(),str2.toCharArray());
        System.out.println(result);
+        int arr[] = {23,10,22,5,33,8,9,21,50,41,60,80,99, 22,23,24,25,26,27};
+        System.out.println(LIS(arr,arr.length));
     }
 }
 
